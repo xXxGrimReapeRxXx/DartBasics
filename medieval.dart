b1d4be9f5358e243bc4cc90bd8ebf2092d8a0060
Acko <ack0@abv.bg>
2024-01-23 23:16:33 +0200

    the idea is to add a knight class and make the code more organized as well to be easier to read and following best practices from early.

--- medieval.dart
+++ medieval.dart
@@ -1,10 +1,51 @@
+import 'dart:io';
+
 void main() {
   print("Hello knight!");
-  print(healthbar(40));
+  print("Current Health: ${healthbar(40)}");
+  print(
+      "Pick your weapon!\n 1. Old axe \n 2. Dragonslayer sword \n 3. A magick rock");
+
+  String? weaponpick = stdin.readLineSync();
+  String? chosenWeapon = myweapon(weaponpick);
+
+  if (chosenWeapon != null) {
+    print("You picked: $chosenWeapon");
+  } else {
+    print("Invalid weapon choice. Please pick a valid option (1, 2, or 3).");
+  }
 }
 
-healthbar(int damage) {
-  int currenthp = 100;
-  currenthp = currenthp - damage;
-  return currenthp;
+int healthbar(int damage) {
+  int currentHP = 100;
+  currentHP -= damage;
+  return currentHP;
+}
+
+int eatmeal() {
+  print("What a delicious stake! I can feel my energy rizing!");
+  currentHP += 10;
+}
+
+String? myweapon(String? weaponpick) {
+  String? weapon;
+
+  switch (weaponpick) {
+    case "1":
+      print("Wow amazing! An axe made from dwarfs!");
+      weapon = "Old axe";
+      break;
+    case "2":
+      print("The legend says it was used to slay 10 dragons...");
+      weapon = "Dragonslayer sword";
+      break;
+    case "3":
+      print("Some wizard forgot about this...");
+      weapon = "Magick rock";
+      break;
+    default:
+      print("Invalid option. Please pick a valid option (1, 2, or 3).");
+  }
+
+  return weapon;
 }
