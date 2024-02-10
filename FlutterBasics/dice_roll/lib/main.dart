ff65ef3d81528d4e6dd4ffe64325c69c3b4e386f
Acko <ack0@abv.bg>
2024-02-10 12:02:48 +0200

    working with gradients

--- FlutterBasics/dice_roll/lib/main.dart
+++ FlutterBasics/dice_roll/lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello")));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(31, 245, 3, 3),
+              Color.fromARGB(66, 100, 53, 8)
+            ],
+          )),
+          child: Center(
+            child: Text("Hello World!"),
+          ),
+        ),
+      ),
+    ),
+  );
 }
